e4aea07d9483265728d6f92bd5b82cac6d541d7e
marco-sanchez-est <79615308+marco-sanchez-est@users.noreply.github.com>
2021-04-30 19:05:16 -0500

    Add files via upload

---
+++ Python/CajeroAutomaticoBasico/main.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+
+#Simulacion de un cajero automatico.
+
+#Definicion de la clase Cliente.
+class Cliente:
+    def __init__(self):
+        #Todos los atributos de la clase cliente son privados.
+        self.__Nombre = " "
+        self.__TipoDeCuenta = " "
+        self.__NoDeCuenta = 0
+        self.__Saldo = 0
+
+    #Todos los metodos de la clase cliente son publicos.
+    def Registro(self, Nombre, TipoDeCuenta, NoDeCuenta, Saldo):
+        self.__Nombre = Nombre
+        self.__TipoDeCuenta = TipoDeCuenta
+        self.__NoDeCuenta = NoDeCuenta
+        self.__Saldo = Saldo
+        return None
+
+    def Retiro(self, monto):
+        self.__Saldo = self.__Saldo - monto
+        return "Saldo final: $" + str(self.__Saldo)
+
+    def Deposito(self, deposito):
+        self.__Saldo = self.__Saldo + deposito
+        return "Saldo final: $" + str(self.__Saldo)
+
+    def ConsultaSaldo(self):
+        return "\nSaldo actual: $" + str(self.__Saldo)
+
+#Definicion de la funcion main.
+def main():
+    cliente1 = Cliente()
+    cliente1.Registro("Pedro Gutierrez", "Premier", 1234567890, 1000)
+    option = 0
+
+    while not(option == 4):
+        print("\n\t\t**Cajero Autom\u00E1tico**\n")
+        print("1. Realizar un retiro")
+        print("2. Realizar un deposito")
+        print("3. Consultar saldo")
+        print("4. Salir\n")
+        option = int(input())
+
+        if option < 1 or option > 4:
+            print("\nERROR: El valor ingresado no es v\u00E1lido\n\n")
+
+        elif option == 1:
+            monto = float(input("\nIngrese la cantidad que desea retirar: "))
+            print("\nUsted ha retirado: $" + str(monto))
+            print(cliente1.Retiro(monto))
+
+        elif option == 2:
+            deposito = float(input("\nIngrese la cantidad que desea depositar: "))
+            print("\nUsted ha depositado: $" + str(deposito))
+            print(cliente1.Deposito(deposito))
+
+        elif option == 3:
+            print(cliente1.ConsultaSaldo())
+
+#Se llama a la funcion main.
+if __name__=="__main__":
+    main()
